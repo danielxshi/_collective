f543f6d12b429f752fc88e62d8c44cbbdf196ffc
Alvin Leung <alvinleung2009@gmail.com>
2022-03-18 22:33:18 -0700

    setup tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import React, { useRef, useEffect } from 'react';
-import { gsap } from 'gsap';
-import NextLink from 'next/link';
+import React, { useRef, useEffect } from "react";
+import { gsap } from "gsap";
+import NextLink from "next/link";
 import {
   Link,
   Container,
@@ -13,10 +13,10 @@ import {
   ListItem,
   Icon,
   useColorModeValue,
-} from '@chakra-ui/react';
-import Navbar from '/components/Navbar/Navbar';
-import Head from 'next/head';
-import IntroMessage from '../components/IntroMessage';
+} from "@chakra-ui/react";
+import Navbar from "/components/Navbar/Navbar";
+import Head from "next/head";
+import IntroMessage from "../components/IntroMessage";
 
 export default function Home() {
   const boxRef = React.createRef();
@@ -27,8 +27,8 @@ export default function Home() {
   useEffect(() => {
     //CURTAIN
     gsap.to(wrapperRef.current, 0.5, {
-      top: '-100%',
-      ease: 'sine.out',
+      top: "-100%",
+      ease: "sine.out",
       delay: 6.6,
     });
 
@@ -36,44 +36,45 @@ export default function Home() {
     gsap.from(boxRef.current, 1.6, {
       scaleY: 0,
       y: 80,
-      ease: 'Power1.easeOut',
-      transformOrigin: '50% 100%',
+      ease: "Power1.easeOut",
+      transformOrigin: "50% 100%",
     });
 
     //LOADER to
     gsap.to(boxRef.current, 1.6, {
       delay: 1.6,
-      height: '20vh',
+      height: "20vh",
       scaleY: 0,
-      ease: 'Power1.easeOut',
-      transformOrigin: '0% -100%',
+      ease: "Power1.easeOut",
+      transformOrigin: "0% -100%",
     });
 
     //HERO to
     gsap.to(heroRef.current, 1.6, {
       opacity: 1,
       delay: 7,
-      ease: 'slow(0.2, 0.7, false)',
-      x: '-=10vw',
+      ease: "slow(0.2, 0.7, false)",
+      x: "-=10vw",
     });
 
     //MEMBER to
     gsap.to(titleRef.current, 1.6, {
       delay: 4,
       opacity: 1,
-      ease: 'slow(0.2, 0.7, false)',
+      ease: "slow(0.2, 0.7, false)",
     });
 
     gsap.to(titleRef.current, 1.6, {
       opacity: 0,
       delay: 6,
-      ease: 'slow(0.2, 0.7, false)',
+      ease: "slow(0.2, 0.7, false)",
     });
   });
 
   return (
     <div>
-      <div className="wrapper" ref={wrapperRef}>
+      <div className="text-white">Test</div>
+      {/* <div className="wrapper" ref={wrapperRef}>
         <div className="loader" ref={boxRef}></div>
         <div className="loader--hero">
           <span className="hero--loader" ref={heroRef}>
@@ -85,7 +86,7 @@ export default function Home() {
         </div>
       </div>
       <Navbar />
-      <div className="grid--container"></div>
+      <div className="grid--container"></div> */}
     </div>
   );
 }
