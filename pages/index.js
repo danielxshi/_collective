4dc5e9c1534288d74a85de3d61334ed52b1cbfa4
danielxshi <danielshi96@gmail.com>
2022-03-19 01:58:35 -0700

    Adding locomotive

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import React, { useRef, useEffect } from "react";
-import { gsap } from "gsap";
-import NextLink from "next/link";
+import React, { useRef, useEffect } from 'react';
+import { LocomotiveScrollProvider } from 'react-locomotive-scroll';
+import { gsap } from 'gsap';
+import NextLink from 'next/link';
 import {
   Link,
   Container,
@@ -12,14 +13,14 @@ import {
   ListItem,
   Icon,
   useColorModeValue,
-} from "@chakra-ui/react";
-import Navbar from "/components/Navbar/Navbar";
-import Head from "next/head";
-import IntroMessage from "../components/IntroMessage";
+} from '@chakra-ui/react';
+import Navbar from '/components/Navbar/Navbar';
+import Head from 'next/head';
+import IntroMessage from '../components/IntroMessage';
 
-import LinkListItem from "../components/LinkList/LinkListItem";
-import LinkList from "../components/LinkList/LinkList";
-import Button from "../components/Button/Button";
+import LinkListItem from '../components/LinkList/LinkListItem';
+import LinkList from '../components/LinkList/LinkList';
+import Button from '../components/Button/Button';
 
 const SectionLabel = ({ children }) => (
   <h2 className="font-mono text-small leading-none pb-4 uppercase tracking-widest opacity-50">
@@ -50,8 +51,8 @@ export default function Home() {
   useEffect(() => {
     //CURTAIN
     gsap.to(wrapperRef.current, 0.5, {
-      top: "-100%",
-      ease: "sine.out",
+      top: '-100%',
+      ease: 'sine.out',
       delay: 6.6,
     });
 
@@ -59,130 +60,148 @@ export default function Home() {
     gsap.from(boxRef.current, 1.6, {
       scaleY: 0,
       y: 80,
-      ease: "Power1.easeOut",
-      transformOrigin: "50% 100%",
+      ease: 'Power1.easeOut',
+      transformOrigin: '50% 100%',
     });
 
     //LOADER to
     gsap.to(boxRef.current, 1.6, {
       delay: 1.6,
-      height: "20vh",
+      height: '20vh',
       scaleY: 0,
-      ease: "Power1.easeOut",
-      transformOrigin: "0% -100%",
+      ease: 'Power1.easeOut',
+      transformOrigin: '0% -100%',
     });
 
     //HERO to
     gsap.to(heroRef.current, 1.6, {
       opacity: 1,
       delay: 7,
-      ease: "slow(0.2, 0.7, false)",
-      x: "-=10vw",
+      ease: 'slow(0.2, 0.7, false)',
+      x: '-=10vw',
     });
 
     //MEMBER to
     gsap.to(titleRef.current, 1.6, {
       delay: 4,
       opacity: 1,
-      ease: "slow(0.2, 0.7, false)",
+      ease: 'slow(0.2, 0.7, false)',
     });
 
     gsap.to(titleRef.current, 1.6, {
       opacity: 0,
       delay: 6,
-      ease: "slow(0.2, 0.7, false)",
+      ease: 'slow(0.2, 0.7, false)',
     });
   });
 
   return (
     <>
-      <div className="wrapper z-0" ref={wrapperRef}>
-        <div className="loader" ref={boxRef}></div>
-        <div className="loader--hero">
-          <span className="hero--loader" ref={heroRef}>
-            _COLLECTIVE
-          </span>
-        </div>
-        <div className="loader--member-wrapper" ref={titleRef}>
-          <IntroMessage />
-        </div>
-      </div>
-      {/* <Navbar /> */}
-      <main className="mx-4 sm:mx-8 2xl:max-w-[96rem] 2xl:mx-auto text-big">
-        <section className="w-full h-screen"></section>
-        <Section>
-          <SectionLabel>About Us</SectionLabel>
-          <BigText>
-            We partner with clients in financial services, electronic commerce,
-            travel and tourism services, entertainment & infotainment, retail
-            industries, CPG, and more.
-          </BigText>
-        </Section>
-        <Section>
-          <div className="flex w-full flex-col items-center">
-            <SectionLabel>Entrusted By</SectionLabel>
-            <LogoContainer>
-              <Logo src={"client-logos/bw/ap.png"} alt="" />
-              <Logo src={"client-logos/bw/calvin-klein.png"} alt="" />
-              <Logo src={"client-logos/bw/fairmont-dubai.png"} alt="" />
-              <Logo src={"client-logos/bw/hublot.png"} alt="" />
-              <Logo src={"client-logos/bw/hunt-fish-club.png"} alt="" />
-              <Logo src={"client-logos/bw/millenium.png"} alt="" />
-            </LogoContainer>
+      <LocomotiveScrollProvider
+        options={{
+          smooth: true,
+          // ... all available Locomotive Scroll instance options
+        }}
+        watch={
+          [
+            //..all the dependencies you want to watch to update the scroll.
+            //  Basicaly, you would want to watch page/location changes
+            //  For exemple, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
+          ]
+        }
+        containerRef={containerRef}
+      >
+        {' '}
+        <div className="wrapper z-0" ref={wrapperRef}>
+          <div className="loader" ref={boxRef}></div>
+          <div className="loader--hero">
+            <span className="hero--loader" ref={heroRef}>
+              _COLLECTIVE
+            </span>
           </div>
-        </Section>
-        <Section>
-          <SectionLabel>Expertises</SectionLabel>
-          <BigText>
-            Hand-picked team,
-            <br /> award-winning results
-          </BigText>
-          <LinkList>
-            <LinkListItem
-              id="1"
-              name="Creative Planning & Marketing"
-              description="Turn your bold vision into bold margin."
-            />
-            <LinkListItem
-              id="2"
-              name="Web Design & Development"
-              description="Turn your bold vision into bold margin."
-            />
-            <LinkListItem
-              id="3"
-              name="Consumer Packaged Goods(CPG)"
-              description="Turn your bold vision into bold margin."
-            />
-            <LinkListItem
-              id="4"
-              name="Videography and Photography"
-              description="Turn your bold vision into bold margin."
-            />
-          </LinkList>
-          <div className="mt-[.5em] flex justify-center md:justify-start md:pl-[25%] w-full">
-            <Button>View More Services</Button>
+          <div className="loader--member-wrapper" ref={titleRef}>
+            <IntroMessage />
           </div>
-        </Section>
-        <footer className="my-[2em]">
-          <div className="flex flex-col items-center md:items-start md:pl-[25%] w-full">
-            <div className="font-serif text-medium mb-5">Got a job for us?</div>
-            <div className="flex flex-col items-center md:items-start mb-8">
-              <a
-                className="font-mono text-medium mb-2"
-                href="mailto:testing@005.com"
-              >
-                testing@005.com
-              </a>
-              <a
-                className="font-mono text-medium"
-                href="mailto:testing@005.com"
-              >
-                +1 (604) 6030483
-              </a>
+        </div>
+        {/* <Navbar /> */}
+        <main data-scroll-container ref={containerRef} className="mx-4 sm:mx-8 2xl:max-w-[96rem] 2xl:mx-auto text-big">
+          <section className="w-full h-screen"></section>
+          <Section>
+            <SectionLabel>About Us</SectionLabel>
+            <BigText>
+              We partner with clients in financial services, electronic
+              commerce, travel and tourism services, entertainment &
+              infotainment, retail industries, CPG, and more.
+            </BigText>
+          </Section>
+          <Section>
+            <div className="flex w-full flex-col items-center">
+              <SectionLabel>Entrusted By</SectionLabel>
+              <LogoContainer>
+                <Logo src={'client-logos/bw/ap.png'} alt="" />
+                <Logo src={'client-logos/bw/calvin-klein.png'} alt="" />
+                <Logo src={'client-logos/bw/fairmont-dubai.png'} alt="" />
+                <Logo src={'client-logos/bw/hublot.png'} alt="" />
+                <Logo src={'client-logos/bw/hunt-fish-club.png'} alt="" />
+                <Logo src={'client-logos/bw/millenium.png'} alt="" />
+              </LogoContainer>
             </div>
-          </div>
-        </footer>
-      </main>
+          </Section>
+          <Section>
+            <SectionLabel>Expertises</SectionLabel>
+            <BigText>
+              Hand-picked team,
+              <br /> award-winning results
+            </BigText>
+            <LinkList>
+              <LinkListItem
+                id="1"
+                name="Creative Planning & Marketing"
+                description="Turn your bold vision into bold margin."
+              />
+              <LinkListItem
+                id="2"
+                name="Web Design & Development"
+                description="Turn your bold vision into bold margin."
+              />
+              <LinkListItem
+                id="3"
+                name="Consumer Packaged Goods(CPG)"
+                description="Turn your bold vision into bold margin."
+              />
+              <LinkListItem
+                id="4"
+                name="Videography and Photography"
+                description="Turn your bold vision into bold margin."
+              />
+            </LinkList>
+            <div className="mt-[.5em] flex justify-center md:justify-start md:pl-[25%] w-full">
+              <Button>View More Services</Button>
+            </div>
+          </Section>
+          <footer className="my-[2em]">
+            <div className="flex flex-col items-center md:items-start md:pl-[25%] w-full">
+              <div className="font-serif text-medium mb-5">
+                Got a job for us?
+              </div>
+              <div className="flex flex-col items-center md:items-start mb-8">
+                <a
+                  className="font-mono text-medium mb-2"
+                  href="mailto:testing@005.com"
+                >
+                  testing@005.com
+                </a>
+                <a
+                  className="font-mono text-medium"
+                  href="mailto:testing@005.com"
+                >
+                  +1 (604) 6030483
+                </a>
+              </div>
+            </div>
+          </footer>
+        </main>
+      </LocomotiveScrollProvider>
     </>
   );
 }
